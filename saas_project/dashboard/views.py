0bc7a1b4e19a74412f744578e07d3ea781919973
Etienne Harode Louembet <Etiennelouembet41@gmail.com>
2026-03-09 00:06:19 +0100

    Add login system(Envoi des mails lors de l'inscription et de la rénitialisation des mots de passe)

--- saas_project/dashboard/views.py
+++ saas_project/dashboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def dashboard_view(request):
+    context={
+        "total_clients":0,
+        "total_commandes":0,
+        "total_produits":0
+    }
+    return render(request, "dashboard/dashboard.html", context)
