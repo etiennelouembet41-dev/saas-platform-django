40309882a352c32b56756b9d86aa57bc66e70197
Etienne Harode Louembet <Etiennelouembet41@gmail.com>
2026-03-10 23:36:33 +0100

    CRUD clients, produits, commandes, factures

--- saas_project/clients/views.py
+++ saas_project/clients/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import ClientSerializer
+from .models import Client
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset=Client.objects.all()
+
+    serializer_class=ClientSerializer
+    
+    permission_classes=[IsAuthenticated]
